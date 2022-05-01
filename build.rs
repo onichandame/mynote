ec453f1360bb49e036e872eb52d457cad56f1931
onichandame <zxinmyth@gmail.com>
2022-05-01 13:19:05 +0000

    chore: try fix build script

--- build.rs
+++ build.rs
@@ -2,14 +2,13 @@ use std::{fs, process::Command};
 
 fn main() {
     println!("compiling spa");
-    let spa_root = fs::canonicalize("./frontend").unwrap();
+    let fe_root = fs::canonicalize(".").unwrap();
+    Command::new("yarn").current_dir(&fe_root).status().unwrap();
     Command::new("yarn")
-        .current_dir(&spa_root)
-        .status()
-        .unwrap();
-    Command::new("yarn")
+        .arg("workspace")
+        .arg("frontend")
         .arg("build")
-        .current_dir(&spa_root)
+        .current_dir(&fe_root)
         .status()
         .unwrap();
     println!("cargo:rerun-if-changed=frontend/src");
