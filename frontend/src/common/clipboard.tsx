b8843ec0fe9e4d7287d0b05715456ed74ae74900
onichandame <zxinmyth@gmail.com>
2022-05-11 12:28:33 +0000

    feat: add clipboard feedback

--- frontend/src/common/clipboard.tsx
+++ frontend/src/common/clipboard.tsx
@@ -1,14 +1,17 @@
 import { ContentCopy } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useSnackbar } from "notistack";
 import { FC } from "react";
 
 /** A button which writes text to clipboard */
 export const Clipboard: FC<{ value: string }> = ({ value }) => {
+  const { enqueueSnackbar } = useSnackbar();
   return (
     <IconButton
-      onClick={async (e) => {
+      onClick={(e) => {
         e.stopPropagation();
         navigator.clipboard.writeText(value);
+        enqueueSnackbar(`copied to clipboard`, { variant: `success` });
       }}
     >
       <ContentCopy />
